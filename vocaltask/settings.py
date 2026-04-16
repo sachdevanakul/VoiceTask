b9d5b52181862cf5bde99bbf8b3990abf68a0b0a
nakul <sachdevanakul813@gmail.com>
2026-04-17 00:06:07 +0530

    fix requirements filename

--- vocaltask/settings.py
+++ vocaltask/settings.py
@@ -90,18 +90,11 @@ WSGI_APPLICATION = 'vocaltask.wsgi.application'
 # ================================
 # DATABASE (Railway PostgreSQL)
 # ================================
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
-
 
 # ================================
 # PASSWORD VALIDATION
